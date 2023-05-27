942f7338238b680cdaa084a3bdf2380885e192be
abdallah-7698 <andro7698@gmail.com>
2023-05-27 12:35:52 +0300

    Initial Commit

---
+++ SwiftGenDemo/SwiftGen/Assets.generated.swift
@@ -0,0 +1,12 @@
+// 🤖 This file was code-generated. Do not edit!!!
+
+import UIKit
+
+// MARK: - Images
+public extension UIImage {
+}
+
+// MARK: - Colors
+public extension UIColor {
+  static let customColor: UIColor = UIColor(named: "custom-color")!
+}
